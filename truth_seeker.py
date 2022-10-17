692866c663f937b7d25dc1df6c915b86ead85013
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-10-17 21:05:47 +0300

    Update requirements.txt & truth_seeker.py

--- truth_seeker.py
+++ truth_seeker.py
@@ -10,6 +10,17 @@ newsapi = NewsApiClient(api_key=API_KEY)
 
 try:
     all_articles = newsapi.get_everything(q="test", sort_by="popularity")
+
+    for k_a, v_a in all_articles.items():
+        print(k_a, v_a)
+        if k_a == "articles":
+            for list_i in v_a:
+                print("=======================")
+                print("NEW NEWS")
+                print("=======================")
+                for k, v in list_i.items():
+                    print(k, v)
+
 except ConnectionError as con_err:
     print(con_err)
 except BaseException as base_err:
@@ -17,12 +28,4 @@ except BaseException as base_err:
 
 # print(all_articles)
 
-for k_a, v_a in all_articles.items():
-    print(k_a, v_a)
-    if k_a == "articles":
-        for list_i in v_a:
-            print("=======================")
-            print("NEW NEWS")
-            print("=======================")
-            for k, v in list_i.items():
-                print(k, v)
+

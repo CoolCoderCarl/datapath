82ff37c3ab5d22ffedd86e45f5aca4fdc83fad9a
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-10-17 21:05:21 +0300

    Update requirements.txt & truth_seeker.py

--- truth_seeker.py
+++ truth_seeker.py
@@ -8,8 +8,12 @@ API_KEY = dynaconfig.config["API_KEY"]
 
 newsapi = NewsApiClient(api_key=API_KEY)
 
-# TRY
-all_articles = newsapi.get_everything(q="test", sort_by="popularity")
+try:
+    all_articles = newsapi.get_everything(q="test", sort_by="popularity")
+except ConnectionError as con_err:
+    print(con_err)
+except BaseException as base_err:
+    print(base_err)
 
 # print(all_articles)
 

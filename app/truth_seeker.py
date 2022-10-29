8fcc05266a10a1114cb259c0299c25f1e0ec9175
CoolCoderCarl <123qwekir2wl@gmail.com>
2022-10-30 02:35:44 +0300

    Add funcs for db & datapath.py to send to Telegram; add requirements for Containerfiles & docker-compose.yaml

--- app/truth_seeker.py
+++ app/truth_seeker.py
@@ -1,9 +1,9 @@
 import logging
-from datetime import date, timedelta
+from datetime import date, datetime, timedelta
 
 from newsapi import NewsApiClient
 
-import databaseconnection
+import db
 import dynaconfig
 
 API_KEY = dynaconfig.settings["API_KEY"]
@@ -13,6 +13,12 @@ QUERY = dynaconfig.settings["QUERY"]
 YESTERDAY = date.today() - timedelta(days=1)
 
 newsapi = NewsApiClient(api_key=API_KEY)
+
+now = datetime.now()
+
+CURRENT_TIME = now.strftime("%H:%M")
+TIME_TO_SEARCH = "01:00"
+
 
 # Logging
 logging.basicConfig(
@@ -41,45 +47,33 @@ def fetch_info() -> dict:
         return {}
 
 
-def list_info(fetch_info: dict):
+def load_to_db(fetch_info: dict):
     if fetch_info:
         for article in fetch_info.get("articles"):
+            article_list = []
             for article_key, article_data in article.items():
-                if article_key == "urlToImage":
-                    continue
-                elif article_key == "source":
-                    print(article_key.capitalize(), article_data.get("name"))
+                if article_key == "source":
+                    pass
+                elif article_key == "content":
+                    pass
+                elif article_key == "urlToImage":
+                    pass
                 else:
-                    print(article_key.capitalize(), article_data)
+                    article_list.append(article_data)
+            db.insert_into(
+                db.create_connection(db.DB_FILE),
+                tuple(article_list),
+            )
     else:
-        logging.warning("Empty response from API")
+        logging.warning("Empty response from API.")
         raise IndexError
 
 
-def load_to_db():
-    for article in fetch_info().get("articles"):
-        article_list = []
-        for article_key, article_data in article.items():
-            if article_key == "source":
-                pass
-            elif article_key == "content":
-                pass
-            elif article_key == "urlToImage":
-                pass
-            else:
-                article_list.append(article_data)
-        article_list.append("True")
-        databaseconnection.insert_into(
-            databaseconnection.create_connection(databaseconnection.DB_FILE),
-            tuple(article_list),
-        )
-
-
 if __name__ == "__main__":
-    if dynaconfig.settings["LOAD_TO_DB"]:
-        try:
-            load_to_db()
-        except BaseException as base_err:
-            logging.error(base_err)
-    else:
-        list_info(fetch_info())
+    while True:
+        if CURRENT_TIME == TIME_TO_SEARCH:
+            logging.info("Time to search has come !")
+            try:
+                load_to_db(fetch_info())
+            except BaseException as base_err:
+                logging.error(base_err)
